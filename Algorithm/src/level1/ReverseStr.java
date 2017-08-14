57b966ca35496936d0a1c3fe030b020001931d2f
youngstone <youngstone@lgcns.com>
2017-08-14 13:09:16 +0900

    "Reverse String with StringBuilder"

--- Algorithm/src/level1/ReverseStr.java
+++ Algorithm/src/level1/ReverseStr.java
@@ -1,21 +1,38 @@
-/*------------------------------------------------------------------------------
- * yeongseok10.kim
- * DESC : 주석문을 보여주기 위하여 임의로 만든 파일
- *        필요한 경우 줄을 늘여 사용할 수 있음 
- * Copyright 2015 LG CNS All rights reserved
- *------------------------------------------------------------------------------
- *                  변         경         사         항                       
- *------------------------------------------------------------------------------
- * 
- *----------------------------------------------------------------------------*/
-
 package level1;
+
+import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.List;
 
 public class ReverseStr {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public String reverseStr(String str){
+		
+//		String [] s = str.split("");
+//		Arrays.sort(s);
+//		List<String> list = Arrays.asList(s);
+//		Collections.reverse(list);
+//		s = list.toArray(new String[list.size()]);
+//	    StringBuilder sb = new StringBuilder();
+//	    
+//	    for (int i = 0; i < s.length; i++) {
+//	    	sb.append(s[i]);
+//		}
+//	    
+//	    
+//		return sb.toString();
+	
+		char[] charray = str.toCharArray();
+		Arrays.sort(charray);
+		return new StringBuilder(new String(charray)).reverse().toString();
+	
 	}
 
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+
+		ReverseStr rs = new ReverseStr();
+		System.out.println( rs.reverseStr("Zbcdefg") );
+	
+	}
 }
