51d3a1a0ca83ec8ca0f838a8a745fb465211f69a
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-02-20 16:11:25 +0900

    linkedlist

--- Algorithm/src/linkedlist/addTwoNumbers.java
+++ Algorithm/src/linkedlist/addTwoNumbers.java
@@ -1,5 +1,6 @@
 package linkedlist;
 
+import java.util.List;
 import java.util.Stack;
 
 public class addTwoNumbers {
@@ -10,55 +11,81 @@ public class addTwoNumbers {
 //        Output: 7 -> 0 -> 8
 //        Explanation: 342 + 465 = 807.
 
-    ListNode n1 = new ListNode(2);
-    n1.next = new ListNode(4);
-    n1.next.next = new ListNode(3);
 
-    ListNode n2 = new ListNode(5);
-    n2.next = new ListNode(6);
-    n2.next.next = new ListNode(4);
+    ListNode n1 = new ListNode(9);
+//    n1.next = new ListNode(4);
+//    n1.next.next = new ListNode(3);
 
-    int result = 0;
-    result = addTwoNumbers(n1,n2);
-    System.out.println(result);
+    ListNode n2 = new ListNode(1);
+    n2.next = new ListNode(9);
+    n2.next.next = new ListNode(9);
+    n2.next.next.next = new ListNode(9);
+    n2.next.next.next.next = new ListNode(9);
+    n2.next.next.next.next.next = new ListNode(9);
+    n2.next.next.next.next.next.next = new ListNode(9);
+    n2.next.next.next.next.next.next.next= new ListNode(9);
+    n2.next.next.next.next.next.next.next.next = new ListNode(9);
+    n2.next.next.next.next.next.next.next.next.next= new ListNode(9);
+
+    ListNode resultNode = addTwoNumbers(n1,n2);
+
 
 
 
     }
 
-    public static int addTwoNumbers(ListNode l1, ListNode l2) {
-        int result = 0;
-        //find number looping through listnode
-        //until next node is not null
-        Stack<Integer> intStack1= new Stack<>();
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        long result = 0;
+
+        Stack<Integer> intStack= new Stack<>();
+        intStack = convertToStack(l1,intStack);
+        result+=convertToNumber(intStack);
+
         Stack<Integer> intStack2= new Stack<>();
+        intStack = convertToStack(l2,intStack2);
+        result+=convertToNumber(intStack);
 
-        int number1=0;
-        int number2=0;
+        String [] string_arr= String.valueOf(result).split("");
+        Stack<Integer> reversedStack = new Stack<>();
+        for (int i = 0; i < string_arr.length; i++) {
+            reversedStack.push(Integer.parseInt(string_arr[i]));
+        }
+        ListNode resultNode = new ListNode(reversedStack.pop());
+        int index = 1;
+        resultNode=rescursiveListNode(resultNode,reversedStack);
 
-        intStack1 = addTwoNumbers.convertToNumber(l1,intStack1);
-        intStack2 = addTwoNumbers.convertToNumber(l2,intStack2);
 
-        result = convertToNumber(intStack1) + convertToNumber(intStack2);
-    return result;
+        return resultNode;
     }
 
-    public static Stack<Integer>  convertToNumber(ListNode node,Stack<Integer> intStack){
+    public static ListNode rescursiveListNode(ListNode node,Stack<Integer> stack){
+
+        if(!stack.empty()){
+
+            node.next = new ListNode(stack.pop());
+
+            rescursiveListNode(node.next,stack);
+        }
+
+        return node;
+    }
+
+    public static Stack<Integer>  convertToStack(ListNode node,Stack<Integer> intStack){
 
         intStack.push(node.val);
         if(node.next != null){
-            addTwoNumbers.convertToNumber(node.next,intStack);
+            convertToStack(node.next,intStack);
         }
 
         return intStack;
     }
-    public static int convertToNumber(Stack<Integer> intStack){
-        int result = 0;
+    public static Long convertToNumber(Stack<Integer> intStack){
+        long result = 0;
         String stringResult = "";
         while(!intStack.empty()){
             stringResult=stringResult.concat(String.valueOf(intStack.pop()));
         }
-        result = Integer.parseInt(stringResult);
+        result = Long.parseLong(stringResult);
         return result;
     }
 
