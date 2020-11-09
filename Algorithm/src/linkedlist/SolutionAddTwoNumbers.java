140ed04fc5c71c953f0a4ecc1db26905cc042b7e
yeongseok.kim <yeongseok.kim@bespinglobal.com>
2020-11-10 00:56:13 +0900

    modified gitignore

--- Algorithm/src/linkedlist/SolutionAddTwoNumbers.java
+++ Algorithm/src/linkedlist/SolutionAddTwoNumbers.java
@@ -2,6 +2,8 @@ package linkedlist;
 
 import linkedlist.addTwoNumbers.ListNode;
 
+import java.util.List;
+
 public class SolutionAddTwoNumbers {
 
 
@@ -9,9 +11,36 @@ public class SolutionAddTwoNumbers {
 
 
 
+
+
+        int [] arr1 = {4,3};
+        ListNode l1 = new ListNode(2);
+        int [] arr2 = {6,4};
+        ListNode l2 = new ListNode(5);
+
+        l1 = generateListNode(l1,arr1);
+        l2 = generateListNode(l2,arr2);
+
+        ListNode node = addTwoNumbers(l1,l2);
+        System.out.println(node);
+
+
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode generateListNode(ListNode rootnode, int [] arr){
+
+        ListNode curr = rootnode;
+        int index = 0;
+        while(index < arr.length){
+            curr.next = new ListNode(arr[index++]);
+            curr = curr.next;
+        }
+        System.out.println(rootnode);
+
+        return rootnode;
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyHead;
         int carry = 0;
@@ -23,7 +52,7 @@ public class SolutionAddTwoNumbers {
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
             if (p != null) p = p.next;
-            if (q != null) q = q.next;
+            if (q != null) q = q.next;7
         }
         if (carry > 0) {
             curr.next = new ListNode(carry);
